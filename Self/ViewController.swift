d4b074af47158aa4eaa00b20b6710fd0792ab464
akimabit <akimabit@gmail.com>
2017-02-13 14:41:49 +0900

    Initial Commit

---
+++ Self/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Self
+//
+//  Created by 金孝錫 on 2017/02/13.
+//  Copyright © 2017年 HYOSUK KIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
